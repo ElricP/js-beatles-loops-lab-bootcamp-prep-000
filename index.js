d629d5ed8a7cfbe1baf7c8ceb4c4e45a815ba80f
Yuan Pan <y39pan@edu.uwaterloo.ca>
2018-08-05 20:23:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 // add solution here
-function 
+function theBeatlesPlay(musicians, instruments){
+  var result = [];
+  for(var counter = 0; counter < musicians.length; counter++){
+    result.push(`${musicians[counter]} plays ${instruments[counter]}`);
+  }
+  return result;
+}
